dbc2e5a7d76162f576b69f707f6308966b29c435
Alex <alexzarodaeli8@gmail.com>
2022-04-12 23:36:27 +0700

    git baru 4

---
+++ OneDrive/Documents/NetBeansProjects/PersonalFinanceRecords/PersonalFinanceRecord/src/model/Warning.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+/**
+ *
+ * @author Alex Zaro Daeli
+ */
+public class Warning {
+    public String Warn(){
+      return "salah bang";
+    }
+
+    public String Error() {
+           return "Masukkan angka dlu cuy";
+   }
+
+    public String More(){
+      return "kelebihan kak";
+    } 
+}
---
+++ OneDrive/Documents/NetBeansProjects/PersonalFinanceRecords/PersonalFinanceRecord/src/personalfinancerecord/PersonalFinanceRecord.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalfinancerecord;
+
+import frame.LoginForm;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PersonalFinanceRecord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginForm login = new LoginForm();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+    
+}
